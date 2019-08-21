9e91673d5d137f5ab3604aa81379d2799ffe8e7b
Noe Fernandes Carvalho Pessoa <noefernandes@A202-07L.imd.local>
2019-08-21 16:34:45 -0300

    Adicionando metodo thread

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import os
 import time
+import threading
 
+#Funcao que efetua a operacao em cada linha da matriz.
 def func(args):
-    sum(args)
+    return sum(args)
 
+#Funcao de auxilio da thread.
+def thread_func(args):
+    t = threading.currentThread()
+    print ("Sou o processo " + str(os.getpid()) + " na thread " + str(t.ident))
+
+#Lista que armazena os resultados retornados da funcao func.
 results = []
-#print("processo pai")
+
+
 def unroll(args, func, method, results):
-    if(method == 'proc'):
-        for i in range(len(args)):
-            if(os.fork() == 0):
-                #print("[son] pid {0} from [parent] pid {1}\n".format(os.getpid(),os.getppid()));
-                results.append(func(args))
+    if method == 'proc':
+        for i in args:
+            results.append(func(i))
+            if os.fork() == 0:
                 os._exit(0)
         for i in range(len(args)):
             os.wait()
-    elif(method == 'thre'):
-    
-unroll([1, 2, 3], func, 'proc', results)
+    elif method == 'thre':
+        for i in args:
+            x = threading.Thread(target=thread_func, args=(i,))
+            x.start()
+            results.append(func(i))
+
+
+unroll([[1,2],[3,4],[5,6]], func, 'thre', results)
+print(results)
+
+#Para testar quantidade de threads ativas.
+#print(threading.activeCount())
