373078e7434dfcc1040af0f70763ab31b33f46b0
Pablo Emanuell Lopes Targino <pabloemanuell@A205-11BL.imd.local>
2019-09-05 13:00:45 -0300

    implementar

---
+++ q3a.py
@@ -0,0 +1,16 @@
+
+''' Para fazer
+- Olhar a seguinte implementação: https://docs.python.org/3/library/queue.html
+- Colocar a operação paralela que já foi feita e utilizar no multiplicar_par
+- Olhar a modelagem na rede de petri e colocar o semaforo
+	
+'''
+
+
+fila_de_matrizes = queue
+
+def multiplicar_par(a, b){
+	# proteger a mémoria
+	# se tiver 1 na fila manda o sinal
+}
+
