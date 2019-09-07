6be4920eed2b705db9e7527296a4e479ba7467fe
Pablo <pabloemanuell2013@hotmail.com>
2019-09-07 14:22:30 -0300

    atualizar modelagem e renomear arquivos

--- q1b1.py
+++ q1b1.py
@@ -9,9 +9,6 @@ import numpy as np
 
 SMEM_NAME = "smlu"
 
-t_start = 0
-t_end = 0
-
 # Cria a mémoria compartilhada
 memory = posix_ipc.SharedMemory(SMEM_NAME, flags = posix_ipc.O_CREAT, mode = 0o777, size = 50)
 mm_results = mmap.mmap(memory.fd, memory.size)
@@ -22,15 +19,16 @@ sem = posix_ipc.Semaphore("test_sem", flags = posix_ipc.O_CREAT, mode = 0o777,  
 
 #Lista que armazena os resultados retornados da funcao func.
 results = []
-height_a = 0
-width_a = 0
-height_b = 0
-width_b = 0
+height = 0
+width = 0
+t_start = 0
+t_end = 0
+
 
 #Funcao que efetua a operacao em cada elemento da matriz em uma thread.
-def proc_func(args, pos):
-	result = sum(args)
-	print(result)
+def proc_func(ele_a, ele_b, i, j):
+	result = ele_a + ele_b
+	pos = (i*width + j)*4
 	# inicio da área protegida
 	sem.acquire()
 	mm_results[pos:pos+4] = result.to_bytes(4,byteorder='big')
@@ -38,67 +36,60 @@ def proc_func(args, pos):
 	# fim da área protegida
 
 #Funcao que efetua a operacao em cada elemento da matriz em um processo
+def thread_func(ele_a, ele_b, results, i, j):
+	results[i][j] = ele_a + ele_b
 
-def thread_aux(results, k, i, j):
-	results[k] = matriz_a[i][k]*matriz_b[k][j]
-
-def thread_func(results, i, j):
-	results_partial = [None] * width_a
-	threads = []
-	for k in range(width_a):
-		x = threading.Thread(target=thread_aux, args=(results_partial, k, i, j))
-		threads.append(x)
-		x.start()
-	for t in threads:
-		t.join()
-	results[i][j] = sum(results_partial)
 
 def unroll(args, func, method, results):
 	len_args = len(args)
 	if method == 'proc':
 		t_start = time.process_time()
-		mm_results.seek(0)
-		for i in range(len_args):
-			if os.fork() == 0:
-				func(args[i], i*4)
-				os._exit(0)
+		for i in range(height):
+			for j in range(width):
+				if os.fork() == 0:
+					func(args[0][i][j], args[1][i][j], i, j)
+					os._exit(0)
 		# Espera todos os processos filho terminarem
-		for _ in range(len_args):
+		for _ in range(width*height):
 			os.waitpid(0, 0)
+		mm_results.seek(0)
+		results = [None] * height
+		for i in range(height):
+			results[i] = [None] * width
+			for j in range(width):
+				results[i][j] = int.from_bytes(mm_results.read(4), byteorder='big')
 		t_end = time.process_time()
-		# Imprime o resultado
-		print('%f' % (t_end - t_start))
-		print([int.from_bytes(mm_results.read(4), byteorder='big') for _ in range(len_args)])
+		print(t_end - t_start)
+		print(results)
 	elif method == 'thre':
 		t_start = time.process_time()
 		threads = []
-		matriz_a = args[0]
-		matriz_b = args[1]
-		results = [None] * height_a
-		for i in range(height_a):
-			results[i] = [None] * width_b
-			for j in range(width_b):
-				x = threading.Thread(target=func, args=(results,i,j))
+		results = [None] * height
+		for i in range(height):
+			results[i] = [None] * width
+
+		for i in range(height):
+			results[i] = [None] * width
+			for j in range(width):
+				x = threading.Thread(target=func, args=(args[0][i][j], args[1][i][j], results, i, j))
 				threads.append(x)
 				x.start()
+
 		for t in threads:
 			t.join()
 		t_end = time.process_time()
+		#print(results)
+
 		print(t_end - t_start)
-		#print('%f' % (t_end - t_start))
 
 if (__name__ == "__main__"):
-	#matriz_a = [[3,2],[3,3], [1,2]]
-	#matriz_b = [[3,2,6],[1,2,5]]
-	#height_a = len(matriz_a)
-	#height_b = len(matriz_b)
-	#width_a = len(matriz_a[0])
-	#width_b = len(matriz_b[0])
-	if(width_a != height_b):
-		print('A matriz não pode ser multiplicada, pois a largura de a:{} != altura de b:{}.'.format(width_a,height_b))
-		exit(1)
+	#matrizA = [[1,2,3],[4,5,6]]
+	#matrizB = [[7,8,9],[10,11,12]]
+	#print(matrizA)
+	#height = len(matrizA)
+	#width = len(matrizA[0])
 
-	#unroll([matriz_a, matriz_b], thread_func, 'thre', results)
+	#unroll([matrizA, matrizB], proc_func, 'proc', results)
 
 	for i in range(101):
 		if(i in [1, 2, 3, 4, 5, 6, 8, 10, 20, 30, 40, 50, 75, 100]):
@@ -110,6 +101,7 @@ if (__name__ == "__main__"):
 	sem.close()
 	mm_results.close()
 	posix_ipc.unlink_shared_memory(SMEM_NAME)
+
 
 #Para testar quantidade de threads ativas.
 #print(threading.activeCount())
--- q3a.py
+++ q3a.py
@@ -1,4 +1,7 @@
-
+# -*- coding: utf-8 -*-
+import threading
+from collections import deque
+from random import randrange
 ''' Para fazer
 - Olhar a seguinte implementação: https://docs.python.org/3/library/queue.html
 - Colocar a operação paralela que já foi feita e utilizar no multiplicar_par
@@ -7,10 +10,66 @@
 '''
 
 
-fila_de_matrizes = queue
+fila_de_matrizes = deque()
 
-def multiplicar_par(a, b){
+def multiplicar_par(a, b, predecessor_lock):
 	# proteger a mémoria
 	# se tiver 1 na fila manda o sinal
-}
 
+	# if(len(deque) > 0): # manda o sinal
+	# predecessor_lock.aquire()
+	# 
+	'''
+	def unroll(args, func, results):
+		threads = []
+
+		results = [None] * height_a
+		for i in range(height_a):
+			results[i] = [None] * width_b
+			for j in range(width_b):
+				x = threading.Thread(target=func, args=(results,i,j))
+				threads.append(x)
+				x.start()
+		for t in threads:
+			t.join()	
+		print(results)
+	from random import randrange'''
+
+def gerar_matriz(height, width, range_start, range_end):
+	matriz = [None]*height
+	for i in range(height):
+		matriz[i] = [None]*width
+		for j in range(width):
+			matriz[i][j] = randrange(range_start, range_end)
+	return matriz
+
+def gerar_matrizes_multiplicativas(amount, min_height = 2, min_width = 2, max_height = 5, max_width = 5,
+									range_start = 0, range_end=10):
+	matrizes = deque()
+	height = randrange(min_height, max_height)
+	for n in range(amount):
+		width = randrange(min_width, max_width)
+		matrizes.append(gerar_matriz(height, width, range_start, range_end))
+		height = width
+	return matrizes
+
+def print_multi_one_digit(matrizes, cols):
+	height = max([len(mat) for mat in matrizes])
+	sep = ' x '
+	for i in range(height):
+		print(sep.join(filter(lambda x: x != None, [
+								str(matrizes[col][i]) if i < len(matrizes[col]) else None 
+								for col in range(cols)])))
+		sep = '   '
+
+if (__name__ == '__main__'):
+	# ABCDE
+	# AB
+	# CD
+	# E
+	# AB CD E
+	# mandar lock do antecessor
+	# gerar conjunto de matrizes
+	# 4x3 * 3x2 * 2x5 * 5*4
+	# 2x3 * 4x2 * 5x2
+	print_multi_one_digit(gerar_matrizes_multiplicativas(2), 2)
