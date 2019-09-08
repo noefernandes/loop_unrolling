c3a94aacf4edf58f3214b75cddb2497a8a4df693
Pablo <pabloemanuell2013@hotmail.com>
2019-09-08 10:31:44 -0300

    sequencial

--- q2a.py
+++ q2a.py
@@ -9,41 +9,24 @@ width_b = 0
 
 def mat_sum(args, results):
 	t_start = time.process_time()
-	matriz_a = args[0]
-	matriz_b = args[1]
-	results = [None] * height_a
+	results = np.zeros((width_a, height_a), dtype=np.uint32)
 	for i in range(height_a):
-		results[i] = [None] * height_a
-		for j in range(height_a):
-			results[i][j] = matriz_a[i][j] + matriz_b[i][j]
+		results[i] = args[0][i] + args[1][i]
 	t_end = time.process_time()
 	print(t_end - t_start)
 
 def mat_product(args, results):
 	t_start = time.process_time()
-	soma = 0
-	matriz_a = args[0]
-	matriz_b = args[1]
-	results = [None] * height_a
+
+	results =  np.zeros((width_a, height_a), dtype=np.uint32)
 	for i in range(height_a):
-		results[i] = [None] * width_b
 		for j in range(width_b):
-			soma = 0
-			for k in range(width_a):
-				soma += matriz_a[i][k]*matriz_b[k][j]
-			results[i][j] = soma
+			results[i] = np.sum(args[0][i]*args[1][:j])
 	t_end = time.process_time()
 	print("tempo: " + str(t_end - t_start))
-	#print("tamanho da matriz: " + str(height_a) + "\n")
-	#print(results)
+
 if (__name__ == "__main__"):
 	results = []
-	#matriz_a = [[3,2],[3,3], [1,2]]
-	#matriz_b = [[3,2,6],[1,2,5]]
-	#height_a = len(matriz_a)
-	#height_b = len(matriz_b)
-	#width_a = len(matriz_a[0])
-	#width_b = len(matriz_b[0])
 	if(width_a != height_b):
 		print('A matriz não pode ser multiplicada, pois a largura de a:{} != altura de b:{}.'.format(width_a,height_b))
 		exit(1)
@@ -53,11 +36,8 @@ if (__name__ == "__main__"):
 		matrizA = np.random.randint(0, np.iinfo(np.uint16).max, (i,i), np.uint16)
 		matrizB = np.random.randint(0, np.iinfo(np.uint16).max, (i,i), np.uint16)
 		height_a, width_a, height_b, width_b = i, i, i, i
-		mat_product([matriz_a, matriz_b],results)
-
-
-#Para testar quantidade de threads ativas.
-#print(threading.activeCount())
+		# mat_sum([matrizA, matrizB],results)
+		mat_product([matrizA, matrizB],results)
 
 
 ''' Referencias
