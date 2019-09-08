42ab743c7b2990bb3cf33bf0168658935d07204a
Pablo <pabloemanuell2013@hotmail.com>
2019-09-07 21:41:29 -0300

    mudança

--- q1b1.py
+++ q1b1.py
@@ -21,18 +21,19 @@ t_end = 0
 #Funcao que efetua a operacao em cada elemento da matriz em uma thread.
 def proc_func(row_a, row_b, i):
 	result = row_a + row_b
-	pos = i*width*4
+	pos = i*width
 	# inicio da área protegida
 	mm_results.seek(pos)
+	bresults = result.tobytes() 
 	sem.acquire()
-	mm_results.write(result.tobytes())
+	mm_results.write(bresults)
 	sem.release()
 	sem.close()
 	# fim da área protegida
 
 #Funcao que efetua a operacao em cada elemento da matriz em um processo
-def thread_func(ele_a, ele_b, results, i, j):
-	results[i][j] = ele_a + ele_b
+def thread_func(row_a, row_b, results, i):
+	results[i] = row_a + row_b
 
 def unroll(args, func, method, results):
 	if method == 'proc':
@@ -52,29 +53,24 @@ def unroll(args, func, method, results):
 			os.waitpid(proc, 0)
 		t_end = time.process_time()
 		sem.acquire()
-		
 		mm_results.seek(0)
 		# Exibir o resultado
-		results = np.zeros((width, height))
-		for i in range(height):
-			for j in range(width):
-				results[i][j] = int.from_bytes(mm_results.read(4), byteorder='big')
-		print(args[0], '+\n', args[1], '=\n', results)
-		
+		results = np.frombuffer(mm_results.read(width*height), dtype=np.uint8, count=width*height) 
+		results.shape = (width,height)
+		# print(args[0], '\n+\n', args[1], '\n=\n', results)
 		print(t_end - t_start)
 	elif method == 'thre':
 		t_start = time.process_time()
 		threads = []
-		results = np.zeros((width, height)).tolist()
+		results = np.zeros((width, height), dtype=np.uint8)
 		for i in range(height):
-			for j in range(width):
-				x = threading.Thread(target=func, args=(args[0][i][j], args[1][i][j], results, i, j))
-				threads.append(x)
-				x.start()
+			x = threading.Thread(target=func, args=(args[0][i], args[1][i], results, i))
+			threads.append(x)
+			x.start()
 		for t in threads:
 			t.join()
 		t_end = time.process_time()
-		# print(args[0], '+', args[1], '=', results)
+		# print(args[0], '\n+\n', args[1], '\n=\n', results)
 		print(t_end - t_start)
 
 if (__name__ == "__main__"):
@@ -84,16 +80,16 @@ if (__name__ == "__main__"):
 	memory.close_fd()
 	sem = posix_ipc.Semaphore(SEM_NAME, flags = posix_ipc.O_CREAT, mode = 0o777,  initial_value=0)
 	# Controla os testes
-	for i in [1, 2, 3, 4, 5, 6, 8, 10]:
-		matrizA = np.random.randint(0,100,(i,i), 'int')
-		matrizB = np.random.randint(0,100,(i,i), 'int')
+	for i in [1, 2, 3, 4, 5, 6, 8, 10, 20, 30, 40, 50, 75, 100]:
+		matrizA = np.random.randint(0,100,(i,i), np.uint8)
+		matrizB = np.random.randint(0,100,(i,i), np.uint8)
 		height, width = i, i
 		unroll([matrizA, matrizB], proc_func, 'proc', results)
+		# unroll([matrizA, matrizB], thread_func, 'thre', results)
 	# Fecha a mémoria
 	sem.unlink()
 	mm_results.close()
 	posix_ipc.unlink_shared_memory(SMEM_NAME)
-
 
 
 ''' Referencias
